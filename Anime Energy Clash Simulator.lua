9b9d689ac49551f0c2b6841f46ceb2d2729381f6
xbladeLOL <142544826+xbladeLOL@users.noreply.github.com>
2023-09-30 21:28:07 +0200

    Add files via upload

---
+++ Anime Energy Clash Simulator.lua
@@ -0,0 +1,56 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "🦒giraffe hub🦒 ",
+    LoadingTitle = "🦒giraffe hub🦒",
+    LoadingSubtitle = "by jiji",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, 
+       FileName = "Big Hub"
+    },
+    Discord = {
+       Enabled = true,
+       Invite = "https://discord.gg/uc8mc5fk8E", 
+       RememberJoins = false 
+    },
+    KeySystem = true, 
+    KeySettings = {
+       Title = "giraffe hub",
+       Subtitle = "Key System",
+       Note = "join the that serv to get key : https://discord.gg/uc8mc5fk8E",
+       FileName = "Key", 
+       SaveKey = true, 
+       GrabKeyFromSite = true, 
+       Key = {"https://pastebin.com/raw/C0F7g3wx"} 
+    }
+ })
+
+local MainTab = Window:CreateTab("🏘️main🏘️") 
+
+local MiscTab = Window:CreateTab("🎲misc🎲")
+
+local WallSection = MainTab:CreateToggle({
+    Name = "auto swing sword",
+    CurrentValue = false,
+    Flag = "Toggle1", 
+    Callback = function(Value)
+    if Value then
+        autoswingsword = true
+
+        while autoswingsword == true do           
+
+            game:GetService("ReplicatedStorage").Remote.Event.Game:FindFirstChild("[C-S]PlayerClick"):FireServer()
+
+            wait()
+            if Value == false then
+                break
+            end
+         end
+    else
+
+        autoswingsword = false
+
+    end
+end,
+ })
