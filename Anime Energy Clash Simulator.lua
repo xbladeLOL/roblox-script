b2e43011a73cfb57dd295ff4ae2862391b333d94
xbladeLOL <142544826+xbladeLOL@users.noreply.github.com>
2023-10-02 16:40:42 +0200

    Update Anime Energy Clash Simulator.lua

--- Anime Energy Clash Simulator.lua
+++ Anime Energy Clash Simulator.lua
@@ -212,33 +212,33 @@ end,
 end,
  })
 
- local WallSection = MainTab:CreateToggle({
-    Name = "auto egg",
-    CurrentValue = false,
-    Flag = "Toggle4", 
-    Callback = function(autoEgg)
-    if autoEgg then
-        autoegg = true
+-- local WallSection = MainTab:CreateToggle({
+--   Name = "auto egg",
+--    CurrentValue = false,
+--    Flag = "Toggle4", 
+--    Callback = function(autoEgg)
+--    if autoEgg then
+--        autoegg = true
 
-        while autoegg == true do      
+ --       while autoegg == true do      
 
-            local args = {
-                [1] = "Egg16"
-            }
-            
-            game:GetService("ReplicatedStorage").Remote.Function.Luck:FindFirstChild("[C-S]DoLuck"):InvokeServer(unpack(args))
+  --          local args = {
+  --              [1] = "Egg16"
+  --          }
+  --          
+  --          game:GetService("ReplicatedStorage").Remote.Function.Luck:FindFirstChild("[C-S]DoLuck"):InvokeServer(unpack(args))
 
-            wait()
-            if autoEgg == false then
-                break
-            end
-         end
-    else
+ --           wait()
+ --           if autoEgg == false then
+ --               break
+ --          end
+ --        end
+ --   else
 
-        autoegg = false
+ --       autoegg = false
 
-    end
-end,
+ --   end
+--end,
  })
 
  local WallSection = MainTab:CreateToggle({
