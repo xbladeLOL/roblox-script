034892d7687d4212f72775d4a24794f9bb1ba43a
xbladeLOL <142544826+xbladeLOL@users.noreply.github.com>
2023-10-07 11:01:49 +0200

    Add files via upload

---
+++ chest hero.lua
@@ -0,0 +1,31 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "🦒giraffe hub🦒 ",
+    LoadingTitle = "🦒giraffe hub🦒",
+    LoadingSubtitle = "by jiji",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, 
+       FileName = "Big Hub"
+    },
+    Discord = {
+       Enabled = true,
+       Invite = "https://discord.gg/uc8mc5fk8E", 
+       RememberJoins = false 
+    },
+    KeySystem = true, 
+    KeySettings = {
+       Title = "giraffe hub",
+       Subtitle = "Key System",
+       Note = "join the that serv to get key : https://discord.gg/uc8mc5fk8E",
+       FileName = "Key", 
+       SaveKey = true, 
+       GrabKeyFromSite = true, 
+       Key = {"https://pastebin.com/raw/C0F7g3wx"} 
+    }
+ })
+
+local MainTab = Window:CreateTab("🏘️main🏘️") 
+
+local MiscTab = Window:CreateTab("🎲misc🎲")
